e01224a9d782e57d2776aebac65b080e3fda4aa0
BHessel <hesselfitness@gmail.com>
2020-10-14 20:03:41 -0400

    codealong complete

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative 'fancy_dance.rb'
+
+class Kid
+    include Dance
+    extend MetaDancing
+    extend Fancy_Dance::ClassMethods 
+    include Fancy_Dance::InstanceMethods
+
+    attr_accessor :name
+    
+    def initialize(name)
+        @name = name
+    end
+    
+end
